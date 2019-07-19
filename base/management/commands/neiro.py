e89bd4ccb9c3a71d17692d14def6e1041596d0f9
Dmitry Shelepnev <mitshel@mail.ru>
2019-07-19 23:14:03 +0300

    Add neiro test

--- base/management/commands/neiro.py
+++ base/management/commands/neiro.py
@@ -1,4 +1,6 @@
-import time
+from keras import models
+from keras import layers
+
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
@@ -21,6 +23,21 @@ class Command(BaseCommand):
 
     def learn(self):
         print('Learning...')
+
+        model = models.Sequential()
+        model.add(layers.Dense(64, activation='relu', input_shape=(6,)))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(46, activation='softmax'))
+
+        model.compile(optimizer='rmsprop',
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        # history = model.fit(x_train,
+        #                     y_train,
+        #                     epochs=20,
+        #                     batch_size=512,
+        #                     validation_data=(x_val, y_val))
 
     def predict(self, emp_UID):
         print('Predict for {}...'.format(emp_UID))
